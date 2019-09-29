db527926751b2fbb88e446b089de154949b37625
zyp <1558255789@qq.com>
2019-09-29 09:38:57 +0800

    add ignore

--- blogv2/settings.py
+++ blogv2/settings.py
@@ -139,9 +139,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -222,4 +220,4 @@ CORS_ALLOW_HEADERS = (
 
 
 # from .local_settings import AK, AKS, QINIU_AK, QINIU_SK
-from .local_settings import DATABASES
+# from .local_settings import DATABASES
